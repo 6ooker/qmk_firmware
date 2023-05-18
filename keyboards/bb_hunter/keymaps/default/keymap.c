b50a00fc36578a578f1059e3429dbb062df552fe
Erik Katzenberger <katzenberger.erik@gmail.com>
2023-05-18 22:12:16 +0200

    added bb_hunter

---
+++ keyboards/bb_hunter/keymaps/default/keymap.c
@@ -0,0 +1,25 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     /*
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      *               ┌───┐           ┌───┐
+      *               │Bsp├───┐   ┌───┤Ent│
+      *               └───┤Tab│   │Spc├───┘
+      *                   └───┘   └───┘
+      */
+    [0] = LAYOUT_default(
+        KC_LSFT,                KC_X,    KC_SPC,
+        KC_LCTL,   KC_A,   KC_S,   KC_D,   KC_F,
+        KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R
+    )
+};
