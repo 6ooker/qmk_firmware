001cab803fc552b7faf08bb63775675cfee9902e
6ooker <katzenberger.erik@gmail.com>
2025-02-10 12:28:45 +0100

    initial setup for southplanck

---
+++ keyboards/southplanck/config.h
@@ -0,0 +1,8 @@
+// Copyright 2022 QMK / James Young (@noroadsleft)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define TRI_LAYER_LOWER_LAYER 1
+#define TRI_LAYER_UPPER_LAYER 2
+#define TRI_LAYER_ADJUST_LAYER 3
